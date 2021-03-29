35d2578d6ce913eb05144dfe87ec43a631b69eb2
ericluuv <ericluuv@gmail.com>
2021-03-29 14:01:33 -0700

    New nav bar put in, temporary border buttons, 2/3 nav buttons working

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+"use strict"
 
+function homeClick()
+{
+    window.location.href = "https://www.ltdfraternity.com";
+}
+
+function aboutClick()
+{
+    window.location.href = "https://www.ltdfraternity.com/about/about.html";
+}
+
+function rushClick()
+{
+    /* Rush not made yet */
+}
