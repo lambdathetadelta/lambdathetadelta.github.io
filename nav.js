b00f514e0353f25f2c73633bda075f38433d6771
ericluuv <ericluuv@gmail.com>
2023-08-01 13:34:27 -0700

    Made philanthrophy page and updated some links to github page for now.

--- nav.js
+++ nav.js
@@ -4,11 +4,11 @@ document.write(`
 <div id="nav-background">
     <nav id="navigation">
         <div id="crest-img">
-            <a href="https://www.ltdfraternity.com">
+            <a href="https://lambdathetadelta.github.io/">
                 <img src="imgs/LTDCrestWhiteV1.png" alt="Crest of Lambda Theta Delta" id="crest-img">
             </a>
         </div>
-        <a href="https://www.ltdfraternity.com" id="ltd-text">
+        <a href="https://lambdathetadelta.github.io/" id="ltd-text">
             <span id="top-text">
                 Lambda Theta Delta
             </span><br>
@@ -18,11 +18,11 @@ document.write(`
         </a>
         <div class="nav-button">
             <button class="nav-button">
-                <a href = "https://www.ltdfraternity.com/about" class="nav-button">About</a>
+                <a href = "https://lambdathetadelta.github.io/about.html" class="nav-button">About</a>
             </button>
             <div class="dropdown-content">
-                <a href="https://www.ltdfraternity.com/history">History</a>
-                <a href="https://www.ltdfraternity.com/philanthropy">Philanthropy</a>
+                <a href="https://lambdathetadelta.github.io/history.html">History</a>
+                <a href="https://lambdathetadelta.github.io/philanthropy.html">Philanthropy</a>
             </div>
         </div>
 
@@ -37,7 +37,7 @@ document.write(`
             </div>
         </div>
 
-        <a href="https://www.ltdfraternity.com#contact" class="nav-button" id="contact-button">Contact</a>
+        <a href="https://lambdathetadelta.github.io#contact" class="nav-button" id="contact-button">Contact</a>
         
         <div id="instagram-img">
             <a href="https://www.instagram.com/lambdathetadelta" target="_blank">
