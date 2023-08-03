d7ae5f3a259248159b5e59d00ccbd851c8acd48f
lambdathetadelta <81211662+lambdathetadelta@users.noreply.github.com>
2023-08-02 18:29:58 -0700

    Updated front page for rush chair info. (#9)
    Co-authored-by: ericluuv <ericluuv@gmail.com>

--- nav.js
+++ nav.js
@@ -47,56 +47,5 @@ navbar.innerHTML = `
     </div>
 </nav>`;
 
-
 const body = document.body;
 body.insertBefore(navbar, body.firstChild);
-
-
-// document.write(`
-// <div id="nav-background">
-//     <nav id="navigation">
-//         <div id="crest-img">
-//             <a href="https://lambdathetadelta.github.io/">
-//                 <img src="imgs/LTDCrestWhiteV1.png" alt="Crest of Lambda Theta Delta" id="crest-img">
-//             </a>
-//         </div>
-//         <a href="https://lambdathetadelta.github.io/" id="ltd-text">
-//             <span id="top-text">
-//                 Lambda Theta Delta
-//             </span><br>
-//             <span id="bottom-text">
-//                 Loyalty. Trust. Diversity.
-//             </span>
-//         </a>
-//         <div class="nav-button">
-//             <button class="nav-button">
-//                 <a href = "https://lambdathetadelta.github.io/about.html" class="nav-button">About</a>
-//             </button>
-//             <div class="dropdown-content">
-//                 <a href="https://lambdathetadelta.github.io/history.html">History</a>
-//                 <a href="https://lambdathetadelta.github.io/philanthropy.html">Philanthropy</a>
-//             </div>
-//         </div>
-
-//         <div class="nav-button">
-//             <button class="nav-button">
-//                 <a href = "https://www.ltdfraternity.com/rush" class="nav-button">Rush</a>
-//             </button>
-//             <div class="dropdown-content">
-//                 <a href="https://www.ltdfraternity.com/anti-hazing">Anti-Hazing</a>
-//                 <a href="https://www.ltdfraternity.com/program">Program</a>
-//                 <a href="https://www.ltdfraternity.com/interest-form">Interest Form</a>
-//             </div>
-//         </div>
-
-//         <a href="https://lambdathetadelta.github.io#contact" class="nav-button" id="contact-button">Contact</a>
-        
-//         <div id="instagram-img">
-//             <a href="https://www.instagram.com/lambdathetadelta" target="_blank">
-//                 <img src="imgs/instagram.png" alt="Instagram Logo" id="instagram-img">
-//             </a>
-//         </div>
-
-//     </nav>
-// </div>
-// `);
