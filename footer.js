194228cbcb567d6680b35b75a6edd751150512ae
ericluuv <ericluuv@gmail.com>
2023-08-02 18:29:02 -0700

    Updated front page for rush chair info.

--- footer.js
+++ footer.js
@@ -1,10 +1,5 @@
 "use strict";
 
-// document.write(`
-// <footer>
-//     <p>Located on the University of California, Irvine Campus. | ltdfraternity@gmail.com </p>
-// </footer>
-// `);
 const footer = document.createElement("footer");
 footer.innerHTML = "<p>Located on the University of California, Irvine Campus. | ltdfraternity@gmail.com </p>";
 document.body.appendChild(footer);
